f43a445f15a87d8e77d80851ac5a37ab6593f919
lichuhub <72883935+lichuhub@users.noreply.github.com>
2025-06-17 13:58:33 -0700

    add graphql query

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,31 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+// Import everything needed to use the `useQuery` hook
+import { useQuery, gql } from '@apollo/client';
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const GetEngagements = gql`
+  query GetEngagements {
+    engagements {
+      items {
+        endDate
+        id
+        internalName
+        lastApprovedDate
+        lastModifiedDate
+        name
+        programName
+        startDate
+        status
+    }
+  }
+}
+`;
+  const { loading, error, data } = useQuery(GetEngagements);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
   return (
     <>
       <div>
@@ -17,6 +38,19 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+       // Display the data from the query
+      <ul>
+        {/* @typescript-eslint/no-explicit-any */},
+        {data.engagements.items.map((engagement: any) => (
+          <li key={engagement.id}>
+            <h2>{engagement.name}</h2>
+            <p>Program: {engagement.programName}</p>
+            <p>Status: {engagement.status}</p>
+            <p>Start Date: {new Date(engagement.startDate).toLocaleDateString()}</p>
+            <p>End Date: {new Date(engagement.endDate).toLocaleDateString()}</p>
+          </li>
+        ))}
+      </ul>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
